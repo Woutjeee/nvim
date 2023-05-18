d6f8dd2265c9fba02a0e4a716bb9b43b6c3aaea2
Wout Hiemstra <wout.hiemstra@hotmail.com>
2023-05-18 13:07:11 +0200

    theme for status line

--- lua/luciferno/packer.lua
+++ lua/luciferno/packer.lua
@@ -38,9 +38,11 @@ use {
         ts_update()
     end,}
 
-    -- Status line & Tab Line
+    -- Status line
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    -- Bufferline
+    use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
 end)
