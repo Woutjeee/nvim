8586b82dfd9d64c74e7f78192d736a09f2de67d1
Wout Hiemstra <wout.hiemstra@hotmail.com>
2023-05-18 09:16:56 +0200

    Added Telescope plugin

--- lua/luciferno/packer.lua
+++ lua/luciferno/packer.lua
@@ -1,5 +1,11 @@
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use { 'wbthomason/packer.nvim' }
-	use { 'marko-cerovac/material.nvim' }
+    -- Packer can manage itself
+    use { 'wbthomason/packer.nvim' }
+    -- Color scheme | Look in after/plugins/colors to see what theme I selected.
+    use { 'marko-cerovac/material.nvim' }
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 end)
