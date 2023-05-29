a933270b8c0a6230c58d4915a53948ee8585801c
Wout Hiemstra <wout.hiemstra@hotmail.com>
2023-05-29 08:27:52 +0200

    forgot to call lsp setup...

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -14,9 +14,9 @@ lsp.nvim_workspace()
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<Up>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<Down>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<CR>'] = cmp.mapping.confirm({ select = false }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
@@ -26,3 +26,5 @@ cmp_mappings['<S-Tab>'] = nil
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
+
+lsp.setup()
