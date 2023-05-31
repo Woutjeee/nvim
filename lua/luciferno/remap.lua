90ad21992b454834fe637e42f932cdc45e28b838
Wout Hiemstra <wout.hiemstra@hotmail.com>
2023-05-31 18:16:54 +0200

    keymaps for bufferline

--- lua/luciferno/remap.lua
+++ lua/luciferno/remap.lua
@@ -6,4 +6,8 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
 vim.keymap.set('n', '<C-h>', '<C-W>h')
 
 -- Prettier
-vim.keymap.set('n', '<leader>f', ':Prettier')
+vim.keymap.set('n', '<leader>f', ':Prettier<cr>')
+
+-- Bufferline
+vim.keymap.set('n', '[b', ':BufferLineCycleNext<CR>')
+vim.keymap.set('n', 'b]', ':BufferLineCyclePrev<CR>')
